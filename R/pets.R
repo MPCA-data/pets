6950e7f81d623b52cc38ed50fe3925745b145523
air <dKvale@users.noreply.github.com>
2020-04-28 08:51:27 -0500

    Add files via upload

---
+++ R/pets.R
@@ -0,0 +1,11 @@
+#' Pets: A package for exclaiming your love of pets.
+#'
+#' The Pets package provides an incredibly important tool
+#' to voice your love of doggies.
+#'
+#' @section Pets functions:
+#' Pets has one function: say_msg()
+#'
+#' @docType package
+#' @name pets
+NULL
---
+++ R/say_msg.R
@@ -0,0 +1,24 @@
+#' A Happy Pet Message
+#'
+#' This function expresses your love of pets.
+#' @param my_fave Your favorite animal. Default = "puppies".
+#' @keywords pets
+#' @export
+#' @examples
+#' say_msg("little dogs")
+#' say_msg()
+
+say_msg <- function(my_fave = "puppies", enthusiasm = 3) {
+
+  # Pet message
+  favorite_pet <- my_fave
+
+  favorite_pet <- toupper(favorite_pet)
+
+  exclaims <- paste(rep("!", enthusiasm), collapse = "")
+
+  msg <- paste0("The best pets in the whole wide world are ", favorite_pet, enthusiasm, " No contest. At all. None.")
+
+  print(msg)
+
+}
